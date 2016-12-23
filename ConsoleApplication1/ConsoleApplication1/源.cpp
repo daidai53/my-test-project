38ea969d592d57382a9bb3f745ccfc4c919da724
李丰庭 <@LI-LAPTOP>
2016-12-24 01:45:22 +0800

    2016-1224-0145

---
+++ ConsoleApplication1/ConsoleApplication1/源.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct LNode
+{
+	int data;
+	struct LNode * next;
+}LNode,* Linklist;
+void GetElem(Linklist L,int i,int &e);
+int main() {
+	return 0;
+}
+void GetElem(Linklist L,int i,int &e) {
+
+}
