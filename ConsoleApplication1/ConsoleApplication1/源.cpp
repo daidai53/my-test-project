a63d9195ba91e2da841726ac6ef91e4851edc9a9
李丰庭 <@LI-LAPTOP>
2016-12-24 13:09:57 +0800

    2016年12月24日13:09:53

--- ConsoleApplication1/ConsoleApplication1/源.cpp
+++ ConsoleApplication1/ConsoleApplication1/源.cpp
@@ -7,12 +7,31 @@ typedef struct LNode
 }LNode,* Linklist;
 void GetElem(Linklist L,int i,int &e);
 void ListInsert(Linklist &L,int i,int e);
+void ListDelete(Linklist &L,int i,int & e);
+void CreateList(Linklist &L,int n);
 int main() {
 	return 0;
 }
 void GetElem(Linklist L,int i,int &e) {
-
+	int j = 1;
+	//while(j!=)
 }
 void ListInsert(Linklist &L, int i, int e) {
 
+}
+void ListDelete(Linklist &L, int i, int & e) {
+
+}
+
+void CreateList(Linklist & L, int n){
+	L = (Linklist)malloc(sizeof(LNode));
+	L->next = NULL;
+	Linklist p;
+	printf("请逆序输入单链表的各个元素:\n");
+	for (int i = 0;i < n;i++) {
+		p = (Linklist)malloc(sizeof(LNode));
+		p->next = L->next;
+		L->next = p;
+		scanf("%d",&p->data);
+	}
 }
