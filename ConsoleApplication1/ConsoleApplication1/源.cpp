f1bbc1bf5ba93a0f2e17355af459c665f0c1bf38
李丰庭 <lifengting95@outlook.com>
2016-12-24 19:38:58 +0800

    2016年12月24日19:38:24 单链表completed

--- ConsoleApplication1/ConsoleApplication1/源.cpp
+++ ConsoleApplication1/ConsoleApplication1/源.cpp
@@ -9,18 +9,64 @@ void GetElem(Linklist L,int i,int &e);
 void ListInsert(Linklist &L,int i,int e);
 void ListDelete(Linklist &L,int i,int & e);
 void CreateList(Linklist &L,int n);
+void print(Linklist L);
 int main() {
+	int n,e;
+	Linklist l;
+	printf("请输入链表长度：");
+	scanf("%d",&n);
+	CreateList(l,n);
+	print(l);
+	GetElem(l,5,e);
+	printf("第五个元素为%d\n",e);
+	ListInsert(l,5,20);
+	print(l);
+	GetElem(l, 5, e);
+	printf("第五个元素为%d\n", e);
+	ListDelete(l,5,e);
+	printf("deleted %d\n",e);
+	print(l);
+	GetElem(l, 5, e);
+	printf("第五个元素为%d\n", e);
 	return 0;
 }
 void GetElem(Linklist L,int i,int &e) {
 	int j = 1;
-	//while(j!=)
+	Linklist p = L->next;
+	while (j != i) {
+		p = p->next;
+		j++;
+	}
+	e = p->data;
 }
 void ListInsert(Linklist &L, int i, int e) {
-
+	Linklist p = L;
+	Linklist s;
+	int j = 0;
+	while (j < i-1) {
+		p = p->next;
+		j++;
+	}
+	if (j <= i) {
+		s = (Linklist)malloc(sizeof(LNode));
+		s->data = e;
+		s->next = p->next;
+		p->next = s;
+	}
+	else {
+		exit(0);
+	}
 }
 void ListDelete(Linklist &L, int i, int & e) {
-
+	Linklist p = L;
+	int j = 0;
+	while (j < i - 1) {
+		j++;
+		p = p->next;
+	}
+	Linklist s = p->next;
+	p->next = s->next;
+	free(s);
 }
 
 void CreateList(Linklist & L, int n){
@@ -34,4 +80,14 @@ void CreateList(Linklist & L, int n){
 		L->next = p;
 		scanf("%d",&p->data);
 	}
+}
+
+void print(Linklist L){
+	Linklist p;
+	p = L->next;
+	while (p != NULL) {
+		printf("%d ",p->data);
+		p = p->next;
+	}
+	printf("\n");
 }
