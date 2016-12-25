990da26da4ed2112998f6f9ae912d5b7e5e3b124
Administrator <Administrator@MS-20161225AKYZ>
2016-12-25 14:39:08 +0800

    2016年12月25日14:38:57 稀疏矩阵的转置

--- ConsoleApplication1/ConsoleApplication1/源.cpp
+++ ConsoleApplication1/ConsoleApplication1/源.cpp
@@ -1,56 +1,57 @@
 #include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
-#define maxsize 100
-typedef struct sqQueue
+using namespace std;
+#define maxsize 1000
+//Ï¡Êè¾ØÕóµÄ×ªÖÃ
+typedef struct triple
 {
-	int * base;
-	int front,rear;
+	int i, j;
+	int e;
 };
-bool Initqueue(sqQueue &q) {
-	q.base = (int *)malloc(maxsize*sizeof(int));
-	if (!q.base)return false;
-	q.front = q.rear = 0;
-	return true;
+typedef struct TSMatrix {
+	triple data[maxsize];
+	int mu, nu, tu;
+};
+
+void print(TSMatrix ts) {
+	int a;
+	int b[100][100];
+	for (int m = 0; m <ts.mu; m++)
+		for (int n = 0; n <ts.nu; n++)
+			b[m][n] = 0;
+	for (a = 0; a < ts.tu; a++) {
+		b[ts.data[a].i][ts.data[a].j] = ts.data[a].e;
+	}
+	for (int m = 0; m < ts.mu; m++) {
+		for (int n = 0; n < ts.nu; n++)
+			printf("%d ", b[m][n]);
+		printf("\n");
+	}
 }
-int length(sqQueue q) {
-	return (q.rear-q.front+maxsize)%maxsize;
-}
-bool enqueue(sqQueue &q,int e) {
-	if ((q.rear + 1) % maxsize == q.front)return false;
-	q.base[q.rear] = e;
-	q.rear = (q.rear + 1) % maxsize;
-	return true;
-}
-bool dequeue(sqQueue &q,int &e) {
-	if (q.front == q.rear)
-		return false;
-	e = q.base[q.front];
-	q.front = (q.front + 1) % maxsize;
-	return true;
+void transpose(TSMatrix ts, TSMatrix &st) {
+	st.mu = ts.nu;
+	st.nu = ts.mu;
+	st.tu = ts.tu;
+	for (int m = 0; m < ts.tu; m++) {
+		st.data[m].e = ts.data[m].e;
+		st.data[m].i = ts.data[m].j;
+		st.data[m].j = ts.data[m].i;
+	}
 }
 int main() {
-	int i, n, t;
-	sqQueue q;
-	if (Initqueue(q))
-		printf("succeed\n");
-	else
-		printf("failed\n");
-	printf("length%d\n",length(q));
-	printf("enter n:");
-	scanf("%d",&n);
-	for (i = 0;i < n;i++) {
-		scanf("%d",&t);
-		if (!enqueue(q, t))
-			printf("full");
-	}printf("length%d\n", length(q));
-	printf("delete the head\n");
-	if (dequeue(q, t)) {
-		printf("%dhas been deleted\n", t);
-		printf("length%d\n", length(q));
-	}
-	else {
-		printf("empty\n");
-		printf("length%d\n", length(q));
-	}
+	TSMatrix ts,st;
+	ts.mu = 5;
+	ts.nu = 3;
+	ts.tu = 2;
+	ts.data[0].e = 3;
+	ts.data[0].i = 1;
+	ts.data[0].j = 2;
+	ts.data[1].e = 1;
+	ts.data[1].i = 0;
+	ts.data[1].j = 0;
+	print(ts);
+	transpose(ts,st);
+	print(st);
 	return 0;
 }
